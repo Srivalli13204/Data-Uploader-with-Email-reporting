c4c5ab72c53e72e6fbe3eaade9ded904d655cd9c
srivalli <101200012+Srivalli13204@users.noreply.github.com>
2024-10-20 17:47:10 +0530

    Add files via upload

---
+++ app/views.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from django.shortcuts import render
+from django.core.mail import EmailMultiAlternatives
+from .forms import UploadFileForm
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+
+# Create your views here.
+def home(request):
+    return render(request, 'app/home.html')
+
+def handle_file(f):
+    df = pd.read_excel(f, engine ='openpyxl') if f.name.endswith('.xlsx') else pd.read_csv(f)
+    return df
+
+def generate_summary(df):
+    summary = {
+        "rows" : df.shape[0],
+        "columns" : df.shape[1],
+    }
+    return summary
+
+def upload(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            file = request.FILES['file']
+            df = handle_file(file)
+            summary = generate_summary(df)
+
+            html = df.to_html(classes='table table-striped', index=False, header=True)
+
+            content = render_to_string('app/email.html', {'summary' : summary, 'html' : html})
+            text = strip_tags(content)
+
+            email = EmailMultiAlternatives(
+                subject='Python Assignment - Pichika Parimala Durga Srivalli',
+                body=text,
+                from_email='isiri1320@gmail.com',
+                to=['isiri132004@gmail.com'],
+            )
+            email.attach_alternative(content, "text/html")
+            email.send()
+
+            context = {
+                'table' : html,
+                'summary' : summary
+            }
+            return render(request, 'app/success.html', context)
+    else:
+        form = UploadFileForm()
+    return render(request, 'app/upload.html', {'form' : form})
